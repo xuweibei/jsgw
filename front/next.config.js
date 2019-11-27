f411e96b4d5e1052266ebd4960778777fe190669
jaylocke <908436244@qq.com>
2019-11-27 11:35:27 +0800

    front 配置

--- front/next.config.js
+++ front/next.config.js
@@ -1,6 +1,16 @@
 const withLess = require('@zeit/next-less')
-module.exports = withLess({
+/*module.exports = withLess({
     lessLoaderOptions: {
         javascriptEnabled: true //开启内联JavaScript
     }
-})
+})*/
+
+module.exports = (phase, defaultConfig) => {
+    console.log('phase', phase);
+    console.log('defaultConfig', defaultConfig);
+    return withLess({
+        lessLoaderOptions: {
+            javascriptEnabled: true //开启内联JavaScript
+        }
+    });
+}
