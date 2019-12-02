c06a04c9f92a4f887595113a72f727b7b8a79ffa
fjutnetworkcxl <cxloong2702@foxmail.com>
2019-12-02 18:32:37 +0800

    网店发货页面

--- back/src/routes/home.js
+++ back/src/routes/home.js
@@ -1,42 +1,11 @@
 module.exports = {
-<<<<<<< HEAD
-    'home': async (ctx, next) => {
-        await ctx.render('index')
-     },
-     'storeDelivery': async (ctx, next) => {
+    "home": async ctx => {
+        await ctx.render('login')
+    },
+    'storeDelivery': async (ctx, next) => {
         await ctx.render('storeDelivery')
      },
      'departmentStructur': async (ctx, next) => {
          await ctx.render('departmentStructur')
      },
-    'login': async ctx => {
-        const {username, password} = ctx.request.body
-        const result = await login(username, password)
-        if (result.username) {
-            const token = jwt.sign({
-                exp: Math.floor(Date.now() / 1000) + (60 * 60),
-                data: result.username
-              }, 'asd12312');
-            ctx.body = new SuccessModel({token}, '登录成功');
-            return;
-        }
-        ctx.body = new ErrorModel('登录失败');
-    },
-    'upload': ctx => {
-        const file = ctx.request.files[Object.keys(ctx.request.files)]
-        const readStream = fs.createReadStream(file.path)
-        const upPath = path.join(__dirname, '../', '../', 'public', 'img', `${file.name}`)
-        // uploadImg(file.path)
-        // 创建写入流
-        const writeStream = fs.createWriteStream(upPath, {encoding: 'utf8'})
-        // 写入文件
-        readStream.pipe(writeStream)
-        ctx.body = {
-            errno: 0
-        }
-=======
-    "home": async ctx => {
-        await ctx.render('login')
->>>>>>> a111b88a19f89c87742e2cbf6181d537a403f6cb
-    }
 }
--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,18 +1,6 @@
 const router = require('koa-router')()
 const home = require('./home')
-<<<<<<< HEAD
-router.get('/', public.root)
-      .get('/test', public.test)
-      .get('/test1', public.test1)
-      .get('/test2', public.test2)
-      .get('/test3', public.test3)
-      .get('/index', public.index)
-      .get('/home', home.home)
-      .post('/login', home.login)
-      .post('/upload', home.upload)
+router.get('/', home.home)
       .get('/delivery/store-delivery', home.storeDelivery)
       .get('/delivery/department-structur', home.departmentStructur)
-=======
-router.get('/', home.home)
->>>>>>> a111b88a19f89c87742e2cbf6181d537a403f6cb
 module.exports = router
