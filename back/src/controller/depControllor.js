128473b86a35b1e3b1198bb7e7af105c042cca29
leijianggitee <18760660507@163.com>
2019-12-12 09:15:51 +0800

    目录修改

--- back/src/controller/depControllor.js
+++ back/src/controller/depControllor.js
@@ -135,7 +135,6 @@ const getEmployee = async () => {
     // return employee
     const sql = "select e.name, e.phone, d.department, i.identity,e.status from gw_employee e left join gw_department d on (e.dep_id=d.id)LEFT JOIN gw_identity i on (i.id=e.ident_id)"
     const ret = await sequelize.query(sql)
-    console.log(ret)
     return ret[0]
 }
 module.exports = {
