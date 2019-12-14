a6670e10ce17975888e34091802d3253ba0ab003
赵志强 <495061502@qq.com>
2019-12-14 17:35:30 +0800

    赵志强

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -12,6 +12,7 @@ const consult = require('./consult')
 const product = require('./product')
 const carousel = require('./carousel')
 const postApi = require('./postApi')
+const helpContent = require('./helpContent')
 router.get('/', login.home)
       .post('/api/login', login.login)
       // 员工部门
@@ -24,6 +25,7 @@ router.get('/', login.home)
       .post('/api/edit_emp', department["edit_emp"])
       .post('/api/del_emp', department["del_emp"])
       .post('/api/change_status', department["change_status"])
+      .post('/api/add_centent_title', department["add_centent_title"])
       // 招聘
       .get('/delivery/invite', invite['invite'])
       // 公司简介
@@ -44,4 +46,6 @@ router.get('/', login.home)
       .get('/delivery/carousel', carousel['turnManage'])
       //职业分类
       .get('/delivery/postClassify', postApi['postClassify'])
+      //产品中心[志强]
+      .get('/delivery/helpContent', helpContent['helpContent'])
 module.exports = router
