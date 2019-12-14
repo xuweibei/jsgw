04a1ed58c1b43ec4228dcd88315844e8817a6d0f
安格 <442477071@qq.com>
2019-12-14 15:08:44 +0800

    antd

--- front/components/layout/layout.jsx
+++ front/components/layout/layout.jsx
@@ -14,12 +14,6 @@ export default ({ children, title = "公司官网" }) => (
                 content="initial-scale=1.0, width=device-width"
             />
             <link rel="icon" href="/favicon.png" />
-            <link
-                rel="stylesheet"
-                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-                crossorigin="anonymous"
-            />
         </Head>
         <Header />
         <div className="content">{children}</div>
--- front/pages/index.jsx
+++ front/pages/index.jsx
@@ -1,9 +1,13 @@
 import ScrollAnim from "rc-scroll-anim"; //滚动动画
 import QueueAnim from "rc-queue-anim"; //进出场动画
+import { DatePicker } from 'antd';
+import moment from 'moment'
 import Layout from "../components/layout/layout";
 import "../static/style/page/index.less";
-import {Form} from 'react-bootstrap';
+
 const ScrollOverPack = ScrollAnim.OverPack;
+const { RangePicker } = DatePicker;
+const dateFormat = 'YYYY-MM-DD';
 
 const Home = () => (
     <Layout>
@@ -12,20 +16,9 @@ const Home = () => (
             <div className="join-one">加入我们</div>
         </div>
         <div className="list distance">
-            <Form.Row>
-                <Form.Group controlId="formGridCity">
-                    <Form.Label>City</Form.Label>
-                    <Form.Control placeholder="请输入职位关键字" />
-                </Form.Group>
-
-                <Form.Group controlId="formGridState">
-                    <Form.Label>State</Form.Label>
-                    <Form.Control as="select">
-                        <option>Choose...</option>
-                        <option>...</option>
-                    </Form.Control>
-                </Form.Group>
-            </Form.Row>
+        <RangePicker
+            defaultValue={[moment('2015-06-06', dateFormat), moment('2015-06-06', dateFormat)]}
+        />
         </div>
     </Layout>
 );
