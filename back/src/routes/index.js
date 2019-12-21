8a974b4ed552c8aa14db98d7d3f525deeb4cb3cb
xuweibei <1582986545@qq.com>
2019-12-21 11:25:20 +0800

    招聘模块预览功能完善

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -29,6 +29,7 @@ router.get('/', login.home)
       .post('/api/get_recruit', resuirc["get_recruit"]) //获取招聘信息
       .post('/api/pcat_bak', resuirc["pcat_bak"]) //获取省市级信息
       .post('/api/add_recruiter', resuirc["add_recruiter"]) //添加招聘信息
+      .post('/api/detelte_recreit', resuirc["detelte_recreit"]) //删除招聘信息
       .post('/api/get_classify', postApi["get_classify"])
       .post('/api/add_centent_title', department["add_centent_title"])
       .post('/api/add_classify',postApi['add_classify'])   //新增职业分类
