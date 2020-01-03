90c41391b17336c8ceeb87731182d842e41b5e14
xuweibei <1582986545@qq.com>
2020-01-03 17:00:48 +0800

    公司招聘搜索优化

--- back/src/controller/recruit.js
+++ back/src/controller/recruit.js
@@ -30,9 +30,9 @@ const getRecruitInfo = async ({post_name = '',job_class='',detail_address='',sta
         }
         if(detail_address != ''){
             if(!where){
-                where =` c.id '${detail_address}'`;
+                where =` c.detail_address = '${detail_address}'`;
             }else{
-                where +=` and c.id '${detail_address}'`;
+                where +=` and c.detail_address = '${detail_address}'`;
             }
         }
         if(start_time != ''){
